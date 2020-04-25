d615e16275cbda9216316f0be4617388ca2cbfc7
Eugene <tatr12345@bk.ru>
2020-04-25 20:13:03 +0300

    Brush

--- script.js
+++ script.js
@@ -5,56 +5,106 @@ let row = document.getElementsByClassName('line'),
     button = document.querySelector('#clean'),
     current = document.querySelector('.current');
 
-    // TO MAKE AN ORDINARRY ARRAY OUT OF THIS ONE
-    coll = [];
-    for(let i = 0; i < col.length; i++){
-        coll.push(col[i]);
-    }
+// TO MAKE AN ORDINARRY ARRAY OUT OF THIS ONE
+coll = [];
+for (let i = 0; i < col.length; i++) {
+    coll.push(col[i]);
+}
 
 let color = '';
 
-lineColor.addEventListener('click', function(e) {
-    switch(e.target.id){
+// CHOOSING THE COLOR AND REMEBERING THAT IN THE VARIABLE
+
+lineColor.addEventListener('click', function (e) {
+    switch (e.target.id) {
         case 'red':
             color = 'red';
+            current.style.background = color;
             break;
         case 'blue':
             color = 'blue';
+            current.style.background = color;
             break;
         case 'green':
             color = 'green';
+            current.style.background = color;
             break;
         case 'yellow':
             color = 'yellow';
+            current.style.background = color;
             break;
         case 'aqua':
             color = '#00FFFF';
+            current.style.background = color;
             break;
         case 'lime':
             color = '#00FF00';
+            current.style.background = color;
             break;
         case 'seagreen':
             color = '#2E8B57';
+            current.style.background = color;
             break;
         case 'magenta':
             color = '#FF00FF';
+            current.style.background = color;
+            break;
+        case 'white':
+            color = 'white';
+            current.style.background = color;
             break;
     }
 });
 
+// Checking if the target is a coll and then add the color to that
 
-container.addEventListener('click', function(e) {
-    if(coll.includes(e.target)){
+/*container.addEventListener('click', function (e) {
+    if (coll.includes(e.target)) {
         e.target.style.background = color;
     }
-});
+}); */
 
-button.addEventListener('click', function() {
-    for(let i of coll){
+// Clean all
+
+button.addEventListener('click', function () {
+    for (let i of coll) {
         i.style.background = 'white';
     }
 });
 
-container.addEventListener('click', function(){
+// Show user which color is being used
+
+
+// PAINTBRUSH
+
+container.addEventListener('mousedown', function (e) {
+    if (coll.includes(e.target)) {
+        e.target.style.background = color;
+        over();
+    }
+    
+});
+
+function over() {
+    container.addEventListener('mouseover', function mouseOver(e){
+        if (coll.includes(e.target)) {
+            e.target.style.background = color;
+        }
+        document.addEventListener('mouseup', function(e){
+            container.removeEventListener('mouseover', mouseOver);
+        });
+    });
+}
+
+
+// INPUT COLOR
+
+let input = document.querySelector('#inputColor');
+
+input.addEventListener('change', function(e){
+    color = e.target.value;
     current.style.background = color;
 });
+
+
+
